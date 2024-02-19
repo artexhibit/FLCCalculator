311fe0c6a6a0c0c82e65056f7005f037a8b6d689
artexhibit <vendabal@yandex.ru>
2024-02-19 22:55:02 +0300

    complete design for Calculation Cell

--- FLCCalculator/CustomViews/Cells/CalculationCell/CalculationView.swift
+++ FLCCalculator/CustomViews/Cells/CalculationCell/CalculationView.swift
@@ -8,23 +8,202 @@ struct CalculationView: View {
     var body: some View {
         
         VStack {
-            HStack(alignment: .center) {
-                Image(.china)
-                .resizable()
-                .frame(width: 20, height: 20)
+            HStack(alignment: .top) {
+                HStack {
+                    Text("1")
+                        .font(.caption.bold())
+                        .foregroundStyle(.bar)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 5)
+                }
+                .background(.accent)
+                .clipShape(Circle())
                 
-                Text("Китай")
-                    .font(.title3.bold())
+                HStack {
+                    Text("12.05.2024")
+                        .font(.caption.bold())
+                        .foregroundStyle(.bar)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 5)
+                }
+                .background(.accent)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Spacer()
+                Image(.china)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+            }
+            
+            HStack {
+                Text("Одежда мужская")
                     .foregroundStyle(.primary)
+                    .bold()
                 
                 Spacer()
             }
-            .padding()
+            .padding(.bottom, 15)
+            
+            HStack(alignment: .top) {
+                VStack(spacing: 5) {
+                    Image(systemName: "circle.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(.accent)
+                    Path { path in
+                        path.move(to: CGPoint(x: 0, y: 0))
+                        path.addLine(to: CGPoint(x: 0, y: 38))
+                    }
+                    .stroke(style: StrokeStyle(lineWidth: 1.5, dash: [3.5]))
+                    .frame(width: 1, height: 38, alignment: .center)
+                    .foregroundStyle(.accent)
+                    Image(systemName: "mappin.and.ellipse")
+                        .resizable()
+                        .frame(width: 18, height: 23)
+                        .foregroundStyle(.accent)
+                }
+                
+                VStack(alignment: .leading, spacing: 30) {
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading) {
+                            Text("Забрать из")
+                                .font(.subheadline)
+                                .foregroundStyle(.secondary)
+                            
+                            Text("Китай, Zhanjiang (Guangdong)")
+                                .font(.callout)
+                                .foregroundStyle(.primary)
+                                .lineLimit(1)
+                        }
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Доставить в")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        
+                        Text("Россия, г.Москва, ул.Носовихинское шоссе, д.19, кв.700")
+                            .font(.callout)
+                            .foregroundStyle(.primary)
+                            .lineLimit(4)
+                    }
+                }
+                
+                Spacer()
+            }
+            
+            VStack {
+                HStack {
+                    HStack {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: "shippingbox.and.arrow.backward.fill")
+                                .resizable()
+                                .frame(width: 20, height: 15)
+                                .foregroundStyle(.accent)
+                            
+                            Text("EXW")
+                                .font(.subheadline)
+                                .foregroundStyle(.orange)
+                                .bold()
+                        }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 10)
+                    }
+                    .background(.accent.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    HStack {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: "scalemass.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .foregroundStyle(.accent)
+                            
+                            Text("450,55 кг")
+                                .font(.subheadline)
+                                .foregroundStyle(.orange)
+                                .bold()
+                        }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 10)
+                    }
+                    .background(.accent.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    HStack {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: "shippingbox.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .foregroundStyle(.accent)
+                            
+                            Text("3 м3")
+                                .font(.subheadline)
+                                .foregroundStyle(.orange)
+                                .bold()
+                        }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 10)
+                    }
+                    .background(.accent.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    HStack {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: "door.garage.closed")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .foregroundStyle(.accent)
+                            
+                            Text("СK Турция - Склад клиента")
+                                .font(.subheadline)
+                                .foregroundStyle(.orange)
+                                .bold()
+                        }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 10)
+                    }
+                    .background(.accent.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    HStack {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: "cart.fill")
+                                .resizable()
+                                .frame(width: 23, height: 19)
+                                .foregroundStyle(.gray)
+                            
+                            Text("1266$ + 17500₽")
+                                .font(.title3)
+                                .foregroundStyle(.primary.opacity(0.8))
+                                .bold()
+                        }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 10)
+                    }
+                    .background(.gray.opacity(0.12))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+               Spacer()
+                }
+                .padding(.top, 15)
+            }
+            .padding(.top, 15)
             Spacer()
             
         }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 16)
         .background(Color(colorScheme == .dark ? .quaternarySystemFill  : UIColor.systemBackground))
-        .clipShape(RoundedRectangle(cornerRadius: 30))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
         .shadow(color: Color(.lightGray).opacity(0.5), radius: 8, x: 0, y: 5)
     }
 }
