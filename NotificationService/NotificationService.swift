db42c8ccced7cffb10c2f0f3872be46272ed631a
artexhibit <vendabal@yandex.ru>
2024-06-18 23:35:15 +0300

    add code to receive keys from push notification in NotificationService extension

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -1,5 +1,9 @@
 import UserNotifications
 
+enum FLCNotificationServiceDataKeys: String {
+    case isCalculationDataAvailable, isDocumentsDataAvailable, isManagerDataAvailable, isNewLogisticsTypesDataAvailable
+}
+
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
@@ -8,21 +12,34 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        guard let bestAttemptContent = bestAttemptContent else { return }
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+        let data = bestAttemptContent.userInfo as NSDictionary
+        
+        for (key, value) in data {
+            guard let keyString = key as? String else { return }
+            guard let dataKey = FLCNotificationServiceDataKeys(rawValue: keyString) else { continue }
+            guard let value = value as? Bool else { continue }
+           
+            if value {
+                switch dataKey {
+                case .isCalculationDataAvailable: break
+                   // Task { await AppDelegateHelper.updateCalculationData(canShowPopup: false) }
+                case .isDocumentsDataAvailable: break
+                   // Task { await AppDelegateHelper.updateDocumentsData() }
+                case .isManagerDataAvailable: break
+                   // Task { await AppDelegateHelper.updateManagerData() }
+                case .isNewLogisticsTypesDataAvailable: break
+                   // Task { await AppDelegateHelper.updateAvailableLogisticsTypesData() }
+                }
+            }
         }
+        contentHandler(bestAttemptContent)
     }
     
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
     }
-
 }
